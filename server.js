f3f9c8d73899b099c4a6c5b75c0200495ae9c0f0
KevinOrosco <kevinorosco411@gmail.com>
2025-05-31 19:12:44 -0300

    Agregue un tiempo limite para las charlas y se menciona con antelacion en cuanto tiempo se cerrara el servidor

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import WebSocket from "ws";
 const wss = new WebSocket.Server({ port: 3000 });
 const clients = new Map();
 
+// enviar un mensaje a todos los clientes conectados al servidor, excepto al que lo envió
 function broadcast(message, senderSocket = null) {
   for (const [client, username] of clients.entries()) {
     if (client.readyState === WebSocket.OPEN && client !== senderSocket) {
@@ -40,3 +41,23 @@ wss.on('connection', (ws) => {
 });
 
 console.log('[Servidor]: Esperando conexiones');
+
+// Notificar cierre en 1 minuto
+setTimeout(() => {
+  broadcast('[Servidor]: El chat se cerrará en 1 minuto.');
+}, 2 * 60 * 1000);
+
+// Cerrar servidor cuando se acaba el tiempo
+setTimeout(() => {
+  broadcast('[Servidor]: El chat se ha cerrado. Desconectando...');
+
+  // Cerrar todas las conexiones
+  for (const [client] of clients.entries()) {
+    client.close();
+  }
+
+  wss.close(() => {
+    console.log('[Servidor]: Chat cerrado.');
+    process.exit(0); // Finaliza el proceso
+  });
+}, 3 * 60 * 1000); // 3 minutos
